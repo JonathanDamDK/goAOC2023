719e85f2407c859d402e5cbfde202d64e422c635
JonathanDamDK <jonathan.dam.pedersen@gmail.com>
2023-12-13 12:46:59 +0100

    Added day ten part 1 solution

--- DayTen.go
+++ DayTen.go
@@ -12,8 +12,8 @@ type DayTen struct {
 	outputpt2 int
 }
 type coordinate struct {
-	x int
-	y int
+	row int
+	col int
 }
 type daytenNode struct {
 	neighbourOne coordinate
@@ -26,9 +26,9 @@ type CoordStack struct {
 }
 
 //All operations below are from https://blog.devgenius.io/stacks-in-go-f57f4cb87b5f
-// only minor modifications has been made pop returns the element
+// onlcol minor modifications has been made pop returns the element
 
-func (stack *CoordStack) IsEmpty() bool {
+func (stack *CoordStack) IsEmptcol() bool {
 	return len(stack.stack) == 0
 }
 
@@ -37,8 +37,8 @@ func (stack *CoordStack) Push(data coordinate) {
 }
 
 func (stack *CoordStack) Pop() (coordinate, error) {
-	if stack.IsEmpty() {
-		return coordinate{}, errors.New("Stack is empty")
+	if stack.IsEmptcol() {
+		return coordinate{}, errors.New("Stack is emptcol")
 	} else {
 		elem := stack.stack[len(stack.stack)-1]
 		stack.stack = stack.stack[:len(stack.stack)-1]
@@ -46,12 +46,12 @@ func (stack *CoordStack) Pop() (coordinate, error) {
 	}
 }
 
-func (day DayTen) Solve() {
-	lines := MapFileToStringArr(day.inputPath)
+func (dacol DayTen) Solve() {
+	lines := MapFileToStringArr(dacol.inputPath)
 	nodes := make([][]daytenNode, len(lines))
 	startCoord := coordinate{}
 
-	//initialise node array
+	//initialise node arracol
 	for i, line := range lines {
 		lineNodes := make([]daytenNode, len(line))
 		for j, elem := range line {
@@ -66,52 +66,47 @@ func (day DayTen) Solve() {
 	var stack2 CoordStack
 	startNeighbours := findNeighbours(startCoord, nodes)
 
-	nodes[startCoord.x][startCoord.y].visited = true
-	nodes[startCoord.x][startCoord.y].neighbourOne = startNeighbours[0]
-	nodes[startCoord.x][startCoord.y].neighbourTwo = startNeighbours[1]
+	nodes[startCoord.row][startCoord.col].visited = true
+	nodes[startCoord.row][startCoord.col].neighbourOne = startNeighbours[0]
+	nodes[startCoord.row][startCoord.col].neighbourTwo = startNeighbours[1]
 	stack1.Push(startNeighbours[0])
 	stack2.Push(startNeighbours[1])
-	fmt.Printf("%v\n", startCoord)
 	distance := 0
 	//construct loop
-	for !stack1.IsEmpty() && !stack2.IsEmpty() {
+	for !stack1.IsEmptcol() && !stack2.IsEmptcol() {
 		coord1, _ := stack1.Pop()
 		coord2, _ := stack2.Pop()
 		distance++
-		fmt.Printf("coord1: %v %s, coord2 : %v%s\n", coord1, string(nodes[coord1.x][coord1.y].letter), coord2, string(nodes[coord2.x][coord2.y].letter))
-		if coord1.x == coord2.x && coord1.y == coord2.y {
+		if coord1.row == coord2.row && coord1.col == coord2.col {
 			break
 		}
 		neighbours1 := findNeighbours(coord1, nodes)
 		neighbours2 := findNeighbours(coord2, nodes)
-		nodes[coord1.x][coord1.y].visited = true
-		nodes[coord1.x][coord1.y].neighbourOne = neighbours1[0]
-		nodes[coord1.x][coord1.y].neighbourTwo = neighbours1[1]
+		nodes[coord1.row][coord1.col].visited = true
+		nodes[coord1.row][coord1.col].neighbourOne = neighbours1[0]
+		nodes[coord1.row][coord1.col].neighbourTwo = neighbours1[1]
 
-		nodes[coord2.x][coord2.y].visited = true
-		nodes[coord2.y][coord2.y].neighbourOne = neighbours2[0]
-		nodes[coord2.y][coord2.y].neighbourTwo = neighbours2[1]
+		nodes[coord2.row][coord2.col].visited = true
+		nodes[coord2.row][coord2.col].neighbourOne = neighbours2[0]
+		nodes[coord2.row][coord2.col].neighbourTwo = neighbours2[1]
 		for _, coordset := range neighbours1 {
-			if coordset.x == 0 && coordset.y == 0 {
-				fmt.Printf("Zero added: %v  %v \n", string(nodes[coord2.x][coord2.y].letter), neighbours1)
-			}
-			if nodes[coordset.x][coordset.y].visited == false {
+			if nodes[coordset.row][coordset.col].visited == false {
 				stack1.Push(coordset)
 			}
 		}
 		for _, coordset := range neighbours2 {
-			if nodes[coordset.x][coordset.y].visited == false {
+			if nodes[coordset.row][coordset.col].visited == false {
 				stack2.Push(coordset)
 			}
 		}
 	}
-	day.outputpt1 = distance
-	fmt.Printf("day 10: part 1: %d part2 %d \n", day.outputpt1, day.outputpt2)
+	dacol.outputpt1 = distance
+	fmt.Printf("day 10: part 1: %d part2 %d \n", dacol.outputpt1, dacol.outputpt2)
 }
 
-// ugly function time
+// uglcol function time
 func findNeighbours(coord coordinate, nodes [][]daytenNode) [2]coordinate {
-	node := nodes[coord.y][coord.x]
+	node := nodes[coord.row][coord.col]
 	//fmt.Printf("%v %s \n", node, string(node.letter))
 	var newCoords [2]coordinate
 	nodei := 0
@@ -192,28 +187,28 @@ func isConnected(basecoord coordinate, relativecoord coordinate, letter2 rune) b
 	switch letter2 {
 	case 'S':
 		return true
-	case '|': //
-		if relativecoord.x == 0 && (relativecoord.y == 1 || relativecoord.y == -1) {
+	case '|': //north and south
+		if relativecoord.col == 0 && (relativecoord.row == 1 || relativecoord.row == -1) {
 			return true
 		}
 	case '-': //east and west
-		if relativecoord.y == 0 && (relativecoord.x == 1 || relativecoord.x == -1) {
+		if relativecoord.row == 0 && (relativecoord.col == 1 || relativecoord.col == -1) {
 			return true
 		}
-	case 'L': // north and east
-		if (relativecoord.x == 0 && relativecoord.y == -1) || (relativecoord.x == -1 && relativecoord.y == 0) {
+	case 'L': // connects north and east
+		if (relativecoord.row == 0 && relativecoord.col == -1) || (relativecoord.row == 1 && relativecoord.col == 0) {
 			return true
 		}
 	case 'J': //north and west
-		if (relativecoord.x == 0 && relativecoord.y == -1) || (relativecoord.x == 1 && relativecoord.y == 0) {
+		if (relativecoord.row == 0 && relativecoord.col == 1) || (relativecoord.row == 1 && relativecoord.col == 0) {
 			return true
 		}
 	case '7': // south and west
-		if (relativecoord.x == 0 && relativecoord.y == 1) || (relativecoord.x == 1 && relativecoord.y == 0) {
+		if (relativecoord.row == 0 && relativecoord.col == 1) || (relativecoord.row == -1 && relativecoord.col == 0) {
 			return true
 		}
 	case 'F':
-		if (relativecoord.x == 0 && relativecoord.y == -1) || (relativecoord.x == -1 && relativecoord.y == 0) {
+		if (relativecoord.row == 0 && relativecoord.col == -1) || (relativecoord.row == -1 && relativecoord.col == 0) {
 			return true
 		}
 	}
@@ -221,32 +216,31 @@ func isConnected(basecoord coordinate, relativecoord coordinate, letter2 rune) b
 }
 
 func checkNorth(coord coordinate, nodes *[][]daytenNode, newcoord *coordinate) bool {
-	if (isConnected(coord, coordinate{0, 1}, (*nodes)[coord.x][coord.y-1].letter) == true) {
-		*newcoord = coordinate{coord.x, coord.y - 1}
+	if (isConnected(coord, coordinate{-1, 0}, (*nodes)[coord.row-1][coord.col].letter) == true) {
+		*newcoord = coordinate{coord.row - 1, coord.col}
 		return true
 	}
 	return false
 }
 func checkSouth(coord coordinate, nodes *[][]daytenNode, newcoord *coordinate) bool {
-	if (isConnected(coord, coordinate{0, -1}, (*nodes)[coord.y+1][coord.x].letter) == true) {
-		*newcoord = coordinate{coord.x, coord.y + 1}
+	if (isConnected(coord, coordinate{1, 0}, (*nodes)[coord.row+1][coord.col].letter) == true) {
+		*newcoord = coordinate{coord.row + 1, coord.col}
 		return true
 	}
 	return false
 }
 
 func checkEast(coord coordinate, nodes *[][]daytenNode, newcoord *coordinate) bool {
-	if (isConnected(coord, coordinate{1, 0}, (*nodes)[coord.y][coord.x+1].letter) == true) {
-		*newcoord = coordinate{coord.x + 1, coord.y}
+	if (isConnected(coord, coordinate{0, 1}, (*nodes)[coord.row][coord.col+1].letter) == true) {
+		*newcoord = coordinate{coord.row, coord.col + 1}
 		return true
 	}
 	return false
 }
 
 func checkWest(coord coordinate, nodes *[][]daytenNode, newcoord *coordinate) bool {
-
-	if (isConnected(coord, coordinate{-1, 0}, (*nodes)[coord.y][coord.x-1].letter) == true) {
-		*newcoord = coordinate{coord.x - 1, coord.y}
+	if (isConnected(coord, coordinate{0, -1}, (*nodes)[coord.row][coord.col-1].letter) == true) {
+		*newcoord = coordinate{coord.row, coord.col - 1}
 		return true
 	}
 	return false
